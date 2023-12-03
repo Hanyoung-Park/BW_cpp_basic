888169f66da423a34fa934ee9e0c83bb15c19510
Hanyoung Park <85710397+Hanyoung-Park@users.noreply.github.com>
2023-12-03 20:34:05 +0900

    Create calculator.cpp
    Uploaded 4_function: Making calculator

---
+++ 4_function/calculator.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int calculator(int num1, string operation, int num2) {
+    int result;
+    // ------------- IMPLEMENT HERE --------------------
+
+    // ------------- IMPLEMENT HERE --------------------
+    return result;
+}
+
+
+int main() {
+    cout << "Your code: " << calculator(1, "+", 4) << endl;
+    cout << "Answer: 5" << endl;
+    cout << endl;
+    cout << "Your code: " << calculator(2, "/", 0) << endl;
+    cout << "Answer: 88888888" << endl;
+    cout << endl;
+    cout << "Your code: " << calculator(3, "*", 27) << endl;
+    cout << "Answer: 81" << endl;
+    cout << endl;
+    cout << "Your code: " << calculator(9, "-", 12) << endl;
+    cout << "Answer: -3" << endl;
+    cout << endl;
+    cout << "Your code: " << calculator(18, "/", 4) << endl;
+    cout << "Answer: 4" << endl;
+    cout << endl;
+    cout << "Your code: " << calculator(18, "a", 4) << endl;
+    cout << "Answer: 88888888" << endl;
+    cout << endl;
+    cout << "Your code: " << calculator(10, "%", 4) << endl;
+    cout << "Answer: 2" << endl;
+    cout << endl;
+    return 0;
+}
